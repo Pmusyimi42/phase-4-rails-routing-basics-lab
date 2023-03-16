d29a3087fcb514781c488bcc1f93da05d5654ea4
patrick kisyula <pmusyimi42@gmail.com>
2023-03-16 04:05:07 +0300

    Assignment Done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        s =  Student.order("grade").reverse
+        render json: s
+    end
+
+    def highest_grade
+        render json: Student.order("grade").reverse.first
+    end
+end
